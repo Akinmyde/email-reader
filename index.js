ec83ea790ce53821e29175147d5700bfe8d7f60d
Olumide Akinremi <akinremiolumide96@gmail.com>
2021-07-23 18:37:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const Imap = require('imap');
+const {simpleParser} = require('mailparser');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+const imapConfig = {
+  user: 'akinremiolumide96@gmail.com',
+  password: 'P@55word$',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true,
+};
+
+const getEmails = () => {
+  try {
+    const imap = new Imap(imapConfig);
+    imap.once('ready', () => {
+      imap.openBox('INBOX', false, () => {
+        imap.search(['UNSEEN', ['SINCE', new Date()]], (err, results) => {
+          const f = imap.fetch(results, {bodies: ''});
+          f.on('message', msg => {
+            msg.on('body', stream => {
+              simpleParser(stream, async (err, parsed) => {
+                // const {from, subject, textAsHtml, text} = parsed;
+                console.log(parsed);
+                /* Make API call to save the data
+                   Save the retrieved data into a database.
+                   E.t.c
+                */
+              });
+            });
+            msg.once('attributes', attrs => {
+              const {uid} = attrs;
+              imap.addFlags(uid, ['\\Seen'], () => {
+                // Mark the email as read after reading it
+                console.log('Marked as read!');
+              });
+            });
+          });
+          f.once('error', ex => {
+            return Promise.reject(ex);
+          });
+          f.once('end', () => {
+            console.log('Done fetching all messages!');
+            imap.end();
+          });
+        });
+      });
+    });
+
+    imap.once('error', err => {
+      console.log(err);
+    });
+
+    imap.once('end', () => {
+      console.log('Connection ended');
+    });
+
+    imap.connect();
+  } catch (ex) {
+    console.log('an error occurred');
+  }
+};
+
+getEmails();
